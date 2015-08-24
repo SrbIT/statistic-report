85c0b7d297fbbb8c1fbc8d7e01321b1b08ee38ec
soh-l <sonpa@hdviet.com>
2015-08-24 14:16:38 +0700

    update v2

---
+++ update_db2.js
@@ -0,0 +1,132 @@
+/**
+ * Created by soh-l on 11/08/2015.
+ * Read data from redis to drop mogodb
+ */
+
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert')
+var ObjectId = require('mongodb').ObjectID;
+var url = 'mongodb://localhost:27017/db_la';
+
+var moment = require('moment');
+var redis = require("redis");
+
+var PORT = 6379,
+//HOST = '127.0.0.1',
+//HOST = '10.0.0.26',
+    HOST = '23.99.96.65',
+
+    client_Redis = redis.createClient(PORT, HOST);
+
+var updateRestaurants = function (paraCollection,
+                                  paraTimeFormat,
+                                  paraObject,
+                                  paraSuff,
+                                  paraTime,
+                                  paraI,
+                                  db, callback) {
+
+
+    var vTimeFormatter, redisKey
+
+    if (paraTime === 'mm') {
+        vTimeFormatter = moment.utc().subtract(paraI, 'minutes').format(paraTimeFormat)
+        console.log(vTimeFormatter)
+
+        redisKey = vTimeFormatter + paraObject
+        console.log(redisKey)
+
+    } else if (paraTime === 'HH') {
+        vTimeFormatter = moment.utc().subtract(paraI, 'hours').format(paraTimeFormat)
+        console.log(vTimeFormatter)
+
+        redisKey = vTimeFormatter + paraObject
+        console.log(redisKey)
+
+    } else if (paraTime === 'dd') {
+        vTimeFormatter = moment.utc().subtract(paraI, 'days').format(paraTimeFormat)
+        console.log(vTimeFormatter)
+
+        redisKey = vTimeFormatter + paraObject
+        console.log(redisKey)
+    }
+
+    client_Redis.hlen(redisKey, function (err, reply) {
+
+        if (reply === null) {
+
+            console.log("Error: " + err)
+            console.log(01)
+
+        } else {
+            console.log(parseInt(reply))
+
+            db.collection(paraCollection).updateOne(
+                {"date_min": vTimeFormatter},
+                {
+                    $set: {"value": parseInt(reply)}
+                },
+                {upsert: true}, function (err, results) {
+                    //console.log(results);
+                    callback();
+                });
+
+        }
+
+    })
+
+};
+
+function UpdateData(paraCollection,
+                    paraTimeFormat,
+                    paraObject,
+                    paraSuff,
+                    paraTime,
+                    paraI) {
+
+    MongoClient.connect(url, function (err, db) {
+        assert.equal(null, err);
+
+        updateRestaurants(paraCollection,
+            paraTimeFormat,
+            paraObject,
+            paraSuff,
+            paraTime,
+            paraI,
+            db, function () {
+                db.close();
+            });
+    });
+
+}
+
+function ConvertData(reply) {
+
+    var keys = Object.keys(reply);
+    var data = [];
+    for (var i = 0; i < keys.length; i++) {
+        var z = {};
+        z.name = keys[i];
+        z.y = parseInt(reply[keys[i]]);
+        data.push(z);
+    }
+    return data
+
+}
+
+var interValUpdate = setInterval(function () {
+
+    for (var i = 35; i >= 5; i--) {
+        UpdateData('tb_sessions_mm', "YYYYMMDDHHmm", ":session:", "_sessions_mm", "mm", i)
+
+    }
+    for (var i = 23; i >= 0; i--) {
+        UpdateData('tb_sessions_HH', "YYYYMMDDHH", ":session:", "_sessions_HH", "HH", i)
+    }
+    for (var i = 1; i >= 0; i--) {
+
+        UpdateData('tb_sessions_dd', "YYYYMMDD", ":session:", "_sessions_dd", "dd", i)
+    }
+
+}, 20000)
+
